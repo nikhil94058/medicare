efde6f9c7464144b857b59b9bb0fddf5873266c2
nikhil94058 <kumarnikhil94058@gmail.com>
2024-01-24 19:57:45 +0530

    DoctorPage 20% complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import { Navbar } from './Component/Navbar';
 import { Hero } from './Component/Hero';
 import Ask from './Component/Ask';
 import { Testimonials } from './Component/Testimonials';
+import { DoctorPage } from './Component/DoctorPage';
 
 
 function App() {
   return (
     <div className="w-[1728px] h-[3737px] relative bg-sky-500 items-center">
-      <p> Ayurmed</p>
+      {/*<p> Ayurmed</p>
       <Navbar />
       <Hero />
       <Ask />
-      <Testimonials />
+      <Testimonials />*/}
+      <DoctorPage />
     </div>
   );
 }
